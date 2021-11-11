08eef715b7af124b6887b1b7f08b2507c9985711
stefanpalmer <palmerstefan@yahoo.com>
2021-11-10 22:37:28 -0700

    Generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { StartComponent } from './start/start.component';
+import { HeaderComponent } from './header/header.component';
+import { NovelComponent } from './novel/novel.component';
+import { AnthologyComponent } from './anthology/anthology.component';
+import { LibraryComponent } from './library/library.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StartComponent,
+    HeaderComponent,
+    NovelComponent,
+    AnthologyComponent,
+    LibraryComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule
