80c804c859584616fefeca43efe7f3d7b743a7f2
Stefan Palmer <stefanpalmer@Stefans-MacBook-Air.local>
2021-11-10 22:37:28 -0700

    Generate components

---
+++ src/app/anthology/anthology.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anthology',
+  templateUrl: './anthology.component.html',
+  styleUrls: ['./anthology.component.css']
+})
+export class AnthologyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
