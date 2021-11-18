33b22c8fafc271727272dfe21120ee861f530330
Stefan Palmer <palmerstefan@yahoo.com>
2021-11-18 02:43:36 -0700

    Add typescript code to save novel data

--- src/app/novel/novel.component.ts
+++ src/app/novel/novel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-novel',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovelComponent implements OnInit {
 
+  novelForm!: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  private initForm() {
+    let novelTitle = '';
+    let novelAuthor = '';
+    let novelPublisher = '';
+    let novelYear = null;
+    let novelPages = null;
+    let novelSeries = '';
+    let novelSeriesNum = null;
+    let novelStories = new FormArray([]);
+    let novelIsbn = '';
+    let novelReview = '';
+    
+    this.novelForm = new FormGroup({
+      'title': new FormControl(novelTitle),
+      'author': new FormControl(novelAuthor),
+      'publisher': new FormControl(novelPublisher),
+      'year': new FormControl(novelYear),
+      'pages': new FormControl(novelPages),
+      'series': new FormControl(novelSeries),
+      'seriesNum': new FormControl(novelSeriesNum),
+      'stories': novelStories,
+      'isbn': new FormControl(novelIsbn),
+      'review': new FormControl(novelReview)
+    })
+    
+  }
 }
